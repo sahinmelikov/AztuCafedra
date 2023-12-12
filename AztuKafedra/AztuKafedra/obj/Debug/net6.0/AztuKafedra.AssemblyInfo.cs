8f45a8eaaaf94ddf41eb0a8d745a7955d72cdb43
MiRoAr <1rima621melikov@gmail.com>
2023-12-13 00:01:40 +0400

    ssssssssssss

--- AztuKafedra/AztuKafedra/obj/Debug/net6.0/AztuKafedra.AssemblyInfo.cs
+++ AztuKafedra/AztuKafedra/obj/Debug/net6.0/AztuKafedra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AztuKafedra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6570d710b1665a28ad7eb2461406b1ae299023e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AztuKafedra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AztuKafedra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
